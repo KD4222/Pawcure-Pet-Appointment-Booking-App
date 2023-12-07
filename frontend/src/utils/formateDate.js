3297db38311c2c6f4c5387d4e1f8daeb92a739a5
KANIKA BANSAL <bansalkanika474@gmail.com>
2023-12-07 13:44:10 +0530

    Initial commit

---
+++ frontend/src/utils/formateDate.js
@@ -0,0 +1,7 @@
+export const formateDate =(date, config) => {
+
+    const defaultOptions = {day:'numeric', month:'short', year:'numeric'}
+    const option = config ? config : defaultOptions
+
+    return new Date(date).toLocaleDateString('en-US', option);
+}
